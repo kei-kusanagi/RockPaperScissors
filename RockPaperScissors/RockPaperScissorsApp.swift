d10232b9bbd2fc9597b075ac7ae38926e97a51a3
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-08-30 19:14:20 -0600

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Juan Carlos Robledo Morales on 30/08/24.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
